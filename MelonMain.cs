a0efc40df8f0939d9052f7a70216d0a899348671
RivinwinAlt <joshuaehillis@yahoo.com>
2023-11-03 20:47:38 -0400

    Initial Upload
    Initial upload of the working mod

---
+++ MelonMain.cs
@@ -0,0 +1,58 @@
+﻿using MelonLoader;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace AVHDayNightMod
+{
+	
+	public class MelonMain : MelonMod
+	{
+		internal static string modFolder = $"{Environment.CurrentDirectory}\\Mods\\{Assembly.GetExecutingAssembly().GetName().Name}";
+
+		public override void OnInitializeMelon()
+		{
+		}
+
+		public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+		{
+			// Create a new material
+			Shader newShader = Shader.Find("Skybox/Cubemap Extended");
+			if (newShader == null)
+			{
+				LoggerInstance.Msg("Shader could not be found");
+				return;
+			}
+			Material newMaterial = new Material(newShader);
+			if (newMaterial == null)
+			{
+				LoggerInstance.Msg("Material could not be created");
+				return;
+			}
+			newMaterial.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f, 1f));
+			newMaterial.SetTexture("_Tex", ReflectionProbe.defaultTexture);
+
+			// Set the skybox material
+			RenderSettings.skybox = newMaterial;
+
+			// Set light intensity
+			GameObject dLight = GameObject.Find("Directional Light");
+			dLight.GetComponent<Light>().intensity = 1f;
+
+			// Set fog111111111111111111111111111111111
+			RenderSettings.fogColor = new Color(0.1764f, 0.8784f, 0.9255f, 1f);
+			RenderSettings.fogDensity = 0.001f;
+		}
+
+		public override void OnUpdate()
+		{
+
+		}
+
+		public override void OnDeinitializeMelon()
+		{
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AVHDayNightMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AVHDayNightMod")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(AVHDayNightMod.MelonMain), "AVHDayNightMod", "1.0.0", "")]
+[assembly: MelonGame("Sayan", "Apes vs Helium")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a9c7e752-33b8-408e-9a50-2d07c7835690")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
